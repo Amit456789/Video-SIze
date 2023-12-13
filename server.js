0551762352e46bc1da28daf3d9f570e5ebbb4c83
Amit Bhandari <amitbhandari@pearlorganisation.com>
2023-12-13 14:40:59 +0530

    Initial xommit

---
+++ server.js
@@ -0,0 +1,25 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+
+let corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
+
+const initRoutes = require("./src/routes");
+
+app.use(express.urlencoded({ extended: true }));
+initRoutes(app);
+
+const port = 8080;
+
+app.get("/", (req, res, next) => {
+  console.log("HELLO WORLD");
+  res.send("HELLO WORLD");
+});
+
+app.listen(port, () => {
+  console.log(`Running at localhost:${port}`);
+});
